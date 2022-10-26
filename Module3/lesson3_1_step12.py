fa455e3440f6a0810685c4fa61af261259680bb8
Sergey Tokarev <nerd@yandex.ru>
2022-10-26 18:19:42 +0300

    Change project structure

---
+++ Module3/lesson3_1_step12.py
@@ -0,0 +1,17 @@
+# Полезные ссылки
+
+# https://learngitbranching.js.org/ — отличный интерактивный туториал
+
+# https://git-scm.com/book/ru/v2/ — лучшая книга вообще 
+
+# https://hyperskill.org/learn/topic/257/﻿
+
+# https://stepik.org/course/4138/﻿
+
+# https://stepik.org/course/3145/
+
+# http://www-cs-students.stanford.edu/~blynn/gitmagic/intl/ru/index.html
+
+# https://habr.com/company/intel/blog/344962/
+
+# https://githowto.com/ru
